1ef0eba840fc18e897cb5fb6a991b025cbeac3d4
allen <isu10203071@yahoo.com.tw>
2021-01-03 13:36:31 +0800

    add 檔案

---
+++ Drinks/Drinks/Dao/Implement/StoreDao.cs
@@ -0,0 +1,62 @@
+﻿using Drinks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Drinks.Dao.Implement
+{
+    public class StoreDao
+    {
+        private Models.DrinksEntities db;
+        public StoreDao() 
+        {
+            db = new DrinksEntities();
+        }
+
+        public void Create(Store store) 
+        {
+            db.Store.Add(store);
+            db.SaveChanges();
+        }
+
+        public List<Store> Get() 
+        {
+            List<Store> result = (from s in db.Store select s).ToList();
+            return result;
+        }
+
+        public void Update(Store store) 
+        {
+            var dbModel = (from s in db.Store where s.Id == store.Id select s).FirstOrDefault();
+
+            if (dbModel != null)
+            {
+                dbModel.Name = store.Name;
+                dbModel.Phone = store.Phone;
+                dbModel.Address = store.Address;
+                dbModel.Note = store.Note;
+                dbModel.DefaultImageId = store.DefaultImageId;
+                db.SaveChanges();
+            }
+        }
+
+        private Store Get(int id) 
+        {
+            return (from s in db.Store
+                    where s.Id == id
+                    select s).FirstOrDefault();
+        }
+
+        public void Delete(Store store) 
+        {
+            var dbModel = Get(store.Id);
+            if (dbModel != null)
+            {
+                db.Store.Remove(dbModel);
+                db.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ Drinks/Drinks/Service/Implement/StoreService.cs
@@ -0,0 +1,42 @@
+﻿using Drinks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Drinks.Service.Implement
+{
+    public class StoreService
+    {
+        private Dao.Implement.StoreDao storeDao;
+        public StoreService() 
+        {
+            storeDao = new Dao.Implement.StoreDao();
+        }
+
+        public Store CreateStore(Store store) 
+        {
+            storeDao.Create(store);
+            return store;
+        }
+
+        public List<Store> GetStores() 
+        {
+            var result = storeDao.Get();
+            return result;
+        }
+
+        public Store UpdateStore(Store store) 
+        {
+            storeDao.Update(store);
+            return store;
+        }
+
+        public Store DeleteStore(Store store) 
+        {
+            storeDao.Delete(store);
+            return store;
+        }
+
+    }
+}
